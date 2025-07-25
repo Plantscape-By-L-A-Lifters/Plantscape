36a4441da8274e6a573dc160af452f02e2bf5766
Justt-Mel <justinmelendezj@gmail.com>
2025-07-25 13:45:25 -0400

    evn.js getting updated

--- env.js
+++ env.js
@@ -1,3 +1,3 @@
 process.env.JWT = "theplantscape";
-// process.env.DATABASE_URL = "postgres://artma:postgres@localhost/plantscape_db";
-process.env.DATABASE_URL = "postgres://chelseacruz@localhost:5432/plantscape";
+ process.env.DATABASE_URL = "postgres://artma:postgres@localhost/plantscape_db";
+
